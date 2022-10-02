cefdcc65a73056a84b34298ba539ebf739954a61
kumaresh <kumareshkumareee75@gmail.com>
2022-10-02 23:31:34 +0530

    213133212

--- src/Sample.java
+++ src/Sample.java
@@ -3,11 +3,11 @@ public class Sample {
 
 	private void m1() {
 		System.out.println("1");
-		System.out.println("Master");
+		System.out.println("Mas\ter");
 	}
 	private void m2() {
 		System.out.println("2");
 		System.out.println("Danny");
-		System.out.println("DOne dan");
+		System.out.println("DOne dn");
 	}
 }
