2537412ad7af8e5751a9b46079c92ca39d54b4fb
kumaresh <kumareshkumareee75@gmail.com>
2022-10-02 23:27:27 +0530

    toooo

--- src/Sample.java
+++ src/Sample.java
@@ -3,10 +3,11 @@ public class Sample {
 
 	private void m1() {
 		System.out.println("1");
-		
+		System.out.println("Master");
 	}
 	private void m2() {
 		System.out.println("2");
-		System.out.println("3");
+		System.out.println("Danny");
+		System.out.println("DOne dan");
 	}
 }
